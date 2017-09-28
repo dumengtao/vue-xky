1f004af19e6e42418638d0d5ca22f9d2a1809669
dumengtao <myxiaodu1994@163.com>
2017-09-28 16:49:13 +0800

    Vue高仿深圳健康网

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,6 @@ import login from '@/components/login/login.vue'
 Vue.use(Router)
 
 export default new Router({
-	mode: 'history',
 	routes: [
 		{ path: '/', redirect: '/xky' },
 		{ path: '/xky', component: homePage },
